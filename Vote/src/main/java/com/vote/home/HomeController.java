ac77f45f394ff4f4b2f9e89cdf535a4da0362f94
inwon <inwon5554@gmail.com>
2016-07-27 13:38:00 +0900

    Homecontroller 55455Welcome으로 변경ㅋ

--- Vote/src/main/java/com/vote/home/HomeController.java
+++ Vote/src/main/java/com/vote/home/HomeController.java
@@ -33,7 +33,7 @@ public class HomeController {
 	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
-		logger.info("44444Welcome home! The client locale is {}.", locale);
+		logger.info("55455Welcome home! The client locale is {}.", locale);
 		
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
