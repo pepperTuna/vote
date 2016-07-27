407fa830b677ccf176eb16204912211830b73a7a
JONGBIN-KIM <kjb7752@gmail.com>
2016-07-27 13:28:58 +0900

    333333

--- Vote/src/main/java/com/vote/home/HomeController.java
+++ Vote/src/main/java/com/vote/home/HomeController.java
@@ -33,7 +33,7 @@ public class HomeController {
 	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
-		logger.info("222222Welcome home! The client locale is {}.", locale);
+		logger.info("33333Welcome home! The client locale is {}.", locale);
 		
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
