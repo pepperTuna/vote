0ceed4a992ebb1363c693011c18cc22b7e96e00a
pepperTuna <age0218@naver.com>
2016-08-01 13:33:08 +0900

    백 160801

--- Vote/src/main/java/com/vote/controller/HomeController.java
+++ Vote/src/main/java/com/vote/controller/HomeController.java
@@ -1,4 +1,4 @@
-package com.vote.home;
+package com.vote.controller;
 
 import java.text.DateFormat;
 import java.util.Date;
--- Vote/src/main/java/com/vote/controller/UploadController.java
+++ Vote/src/main/java/com/vote/controller/UploadController.java
@@ -1,4 +1,4 @@
-package com.vote.home;
+/*package com.vote.controller;
 
 import java.io.File;
 import java.util.UUID;
