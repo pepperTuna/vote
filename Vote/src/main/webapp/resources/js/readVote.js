0a5a03ff65aaba6a8d1255fe8927ccb8254bc51c
JONGBIN-KIM <kjb7752@gmail.com>
2016-08-18 17:20:08 +0900

    result 중간 커밋

--- Vote/src/main/webapp/resources/js/readVote.js
+++ Vote/src/main/webapp/resources/js/readVote.js
@@ -28,6 +28,37 @@ $(document).ready(function(){
 	
 	$('#btn_submit').on('click', function(e) {
 		if(confirm("투표를 마치시겠습니까?") == true){
+			
+			var map = new Object();
+			
+			var qLength = $('.question-title').length;
+			map.length = qLength;
+			
+			for(var i=1;i<=qLength;i++){
+				var checked = $('input:radio[class="radio-'+i+'"]:checked').attr('name');
+
+				if(checked == null){
+					return;
+				}else{
+					var index = checked.substring(checked.lastIndexOf('-')+1, checked.length);
+					map[''+i] = index;
+				}
+				
+			}
+			
+			var jsonString = JSON.stringify(map);
+			
+			$.ajax({
+				url:'/createResult',
+				contentType : 'application/json;charset=UTF-8',
+				type:'post',
+				dataType:'json',
+				data:jsonString,
+				success:function(data){
+					console.log(data);
+				}
+			});			
+			
 			alert("투표를 완료했습니다.");
 		} else {
 			return;
