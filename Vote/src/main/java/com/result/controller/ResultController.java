c27001577dbf0984c7e97dc54947a6d24b410e86
JONGBIN-KIM <kjb7752@gmail.com>
2016-08-19 15:10:01 +0900

    하!!

--- Vote/src/main/java/com/result/controller/ResultController.java
+++ Vote/src/main/java/com/result/controller/ResultController.java
@@ -5,16 +5,15 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import javax.inject.Inject;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.fasterxml.jackson.core.JsonParseException;
-import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.result.domain.ResultVO;
@@ -22,7 +21,8 @@ import com.result.service.ResultService;
 
 @Controller
 public class ResultController {
-	@Autowired
+	
+	@Inject
 	ResultService resultService;
 	
 	@RequestMapping(value="/readResult", method=RequestMethod.GET)
@@ -32,13 +32,21 @@ public class ResultController {
 		model.addAttribute("resultList", resultList);
 	}
 	
+	@RequestMapping(value="/createResult", method=RequestMethod.GET)
+	public String createResultGet(){
+		
+		return "hello222";
+	}
+	
 	@RequestMapping(value="/createResult", method=RequestMethod.POST)
-	public String createResult(@RequestBody String jsonStr) throws JsonParseException, JsonMappingException, IOException{
+	public String createResult(/*@RequestBody String jsonStr*/) throws JsonParseException, JsonMappingException, IOException{
 		ObjectMapper mapper = new ObjectMapper();
 		Map<String, String> map = new HashMap<String, String>();
 		
-		map = mapper.readValue(jsonStr, new TypeReference<Map<String, String>>(){});
+		//map = mapper.readValue(jsonStr, new TypeReference<Map<String, String>>(){});
 		
-		return map.get("length");
+		//return map.get("length");
+		return "hello";
 	}
+	
 }
